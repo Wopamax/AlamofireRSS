4f5b1f4660f022e32d48c8a1ff09eaf3c539acdf
Maxime de Chalendar <maxime.dechalendar@wopata.com>
2016-03-16 10:58:33 +0100

    Made the RSSFeed and RSSItem variables public

--- Pod/Classes/RSSItem.swift
+++ Pod/Classes/RSSItem.swift
@@ -9,14 +9,14 @@
 import Foundation
 
 public class RSSItem: CustomStringConvertible {
-    var title: String? = nil
-    var link: String? = nil
-    var itemDescription: String? = nil
-    var guid: String? = nil
-    var author: String? = nil
-    var comments: String? = nil
-    var source: String? = nil
-    var pubDate: NSDate? = nil
+    public var title: String? = nil
+    public var link: String? = nil
+    public var itemDescription: String? = nil
+    public var guid: String? = nil
+    public var author: String? = nil
+    public var comments: String? = nil
+    public var source: String? = nil
+    public var pubDate: NSDate? = nil
 
     public var description: String {
         return "title: \(self.title)\nlink: \(self.link)\nitemDescription: \(self.itemDescription)\nguid: \(self.guid)\nauthor: \(self.author)\ncomments: \(self.comments)\nsource: \(self.source)\npubDate: \(self.pubDate)"
